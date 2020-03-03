4a62250545760589c76da94ed218b7e201832d37
Rafael Santos <rsantoslfap@gmail.com>
2020-03-03 08:12:37 -0300

    Aula 12

---
+++ script.js
@@ -0,0 +1,17 @@
+var lista = ['Rafael','Ian', 'Guilherme', 'William'];
+
+alert (lista);
+alert(lista.indexOf("Rafael"));
+const CPO = lista[2];
+alert('O CPO da DMZ e: '+ CPO);
+
+alert(lista.join('+'));
+alert(lista.pop());
+alert(lista.shift());
+alert(lista.indexOf("Guilherme"));
+lista[0]= "Ian Canario";
+lista[1]= "Guilherme Bafica";
+lista.push("Rafael Santos", "William Soares");
+lista.toString();
+
+alert(lista);
